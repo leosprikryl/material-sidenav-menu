4deedea26541c2cdf1c11d136b042f6b7f86ed53
olimou <olimou@gmail.com>
2015-06-24 20:47:03 -0300

    add how to use example

--- src/main/resources/assets/js/app.js
+++ src/main/resources/assets/js/app.js
@@ -3,17 +3,7 @@ var module = angular.module('App', ['ngMaterial', 'ngMenuSidenav']);
 module.controller('AppCtrl', ['$scope', '$mdSidenav', function ($scope, $mdSidenav) {
     $scope.index = 0;
 
-    console.log('create controller');
-
     $scope.toggleSidenav = function (menuId) {
         $mdSidenav(menuId).toggle();
-    };
-
-    $scope.ola = function () {
-        window.alert("ola");
-    };
-
-    $scope.create = function () {
-        console.log($scope.index++);
     };
 }]);
